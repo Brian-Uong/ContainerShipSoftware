8471526545c194e764f3303ad1c91aae83bf0b67
Evan de Chambeau <edechambeau1@gmail.com>
2024-11-11 20:12:22 -0800

    Added implementation for logging and updated the UI on the home page.

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+from datetime import datetime
 
 def create_app():
     app = Flask(__name__)
@@ -8,5 +9,30 @@ def create_app():
     from .auth import auth
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    @app.route('/')
+    def home():
+        return "Home Page"
+
+    @app.route('/log', methods=['POST'])
+    def log_message():
+        log_message = request.form.get('logMessage')
+        file_path = 'log.txt'
+
+        with open(file_path, 'a') as files:
+            files.write(datetime.now().strftime('%Y-%m-%d %H:%M') + ' ' + log_message + '\n')
+
+        return redirect(url_for('home'))
+    
+    @app.route('/signIn', methods=['POST'])
+    def sign_in():
+        sign_in = request.form.get('empName')
+        file_path = 'log.txt'
+
+        with open(file_path, 'a') as files:
+            files.write(datetime.now().strftime('%Y-%m-%d %H:%M') + ' ' + sign_in + ' signs in.\n')
+
+        return redirect(url_for('home'))
+    
 
     return app
