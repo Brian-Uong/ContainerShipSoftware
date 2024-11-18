fb3accab2b99fa1d860255d50cdf1360c16fd51c
Evan de Chambeau <edechambeau1@gmail.com>
2024-11-17 23:40:40 -0800

    Started setup for the grids

--- Website/__init__.py
+++ Website/__init__.py
@@ -41,4 +41,12 @@ def create_app():
     def unload_loadRedirect():
         return redirect(url_for('auth.unload_load'))
 
+
+    #Need to create something that will cause a file explorer pop-up when the user goes to the balance page or the unload/load page.
+    #I think how I will implement this is add a folder that holds the manifest. When the page loads it should check if that folder is empty.
+    #If it is empty it will bring up the file explorer pop-up. If it isn't empty it will just load the page with that manifest. This should 
+    #enable it to keep working if the power goes out. When the user says they have completed the cycle it will delete the manifest file from the manifest folder.
+
+    
+
     return app
