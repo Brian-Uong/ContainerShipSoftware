d332daafdd857f1d60a8f41296b0ec8e38566aa6
edechambeau1 <91441455+edechambeau1@users.noreply.github.com>
2024-11-21 19:38:00 -0800

    Reworked the log message/sign in functionality to use sessions and set up home page.

--- Website/auth.py
+++ Website/auth.py
@@ -1,21 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, url_for
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/')
+def home():
+    session['previous_url'] = url_for('home')
+    return render_template("home.html")
+
 @auth.route('/balance')
 def balance():
+    session['previous_url'] = url_for('auth.balance')
     return render_template("balancing.html")
 
 @auth.route('/unload_load')
 def unload_load():
+    session['previous_url'] = url_for('auth.unload_load')
     return render_template("unload_load.html")
 
-@auth.route('/signin')
-def signin():
-    return render_template("sign_in.html")
+# @auth.route('/signin')
+# def signin():
+#     return render_template("sign_in.html")
 
 @auth.route('/file_upload')
 def file_upload():
+    session['previous_url'] = url_for('file_upload')
     return render_template("file_upload.html")
 
 
--- Website/views.py
+++ Website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, url_for
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
+    session['previous_url'] = url_for('home')
     return render_template("home.html")
