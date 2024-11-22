9da54565ef87ffe1e4002bf04c1108bfb5078c7f
edechambeau1 <91441455+edechambeau1@users.noreply.github.com>
2024-11-22 04:45:25 -0800

    Properly uploads manifest file into a manifest folder. Still needs to be parsed when it arrives there.

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, redirect, url_for, session
+import os
+from flask import Flask, request, redirect, url_for, session, flash
+from werkzeug.utils import secure_filename
 from datetime import datetime
 
 def create_app():
+
+    UPLOAD_FOLDER = '/'
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret key BEAM'
 
@@ -25,27 +30,6 @@ def create_app():
 
         return redirect(session['previous_url'])
     
-    # Crated 2 more log functions so user can stay on page where message was logged
-    # @app.route('/logBalance', methods=['POST'])
-    # def log_message_balance():
-    #     log_message = request.form.get('logMessage')
-    #     file_path = 'log.txt'
-
-    #     with open(file_path, 'a') as files:
-    #         files.write(datetime.now().strftime('%Y-%m-%d %H:%M') + ' ' + log_message + '\n')
-
-    #     return redirect(url_for('auth.balance'))
-    
-    # @app.route('/logUnloadLoad', methods=['POST'])
-    # def log_message_UnloadLoad():
-    #     log_message = request.form.get('logMessage')
-    #     file_path = 'log.txt'
-
-    #     with open(file_path, 'a') as files:
-    #         files.write(datetime.now().strftime('%Y-%m-%d %H:%M') + ' ' + log_message + '\n')
-
-    #     return redirect(url_for('auth.unload_load'))
-    
     @app.route('/signIn', methods=['POST'])
     def sign_in():
         sign_in = request.form.get('empName')
@@ -57,16 +41,34 @@ def create_app():
         return redirect(session['previous_url'])
     
     @app.route('/homeRedirect', methods=['POST'])
-    def homeRedirect():
+    def homeRedirect():    
         session['previous_url'] = url_for('home')
         return redirect(url_for('auth.home'))
 
-    @app.route('/balanceRedirect', methods=['POST'])
+    #These both take a file from their respective buttons on the home page and upload it to the ManifestFolder file. If there is no file selected it will keep them on the hope page.
+    @app.route('/balanceRedirect', methods=['GET','POST'])
     def balanceRedirect():
+        print(request.files)
+        file = request.files['manifest-input-balance']
+        filename = secure_filename(file.filename)
+        if filename == '': 
+            session['previous_url'] = url_for('home')
+            return redirect(url_for('home'))
+        if file:
+            file.save(os.path.join(app.root_path+'\ManifestFolder', filename))
         return redirect(url_for('auth.balance'))
+    
     
     @app.route('/unload_loadRedirect', methods=['POST'])
     def unload_loadRedirect():
+        print(request.files)
+        file = request.files['manifest-input-unload-load']
+        filename = secure_filename(file.filename)
+        if filename == '':
+            session['previous_url'] = url_for('home')
+            return redirect(url_for('home'))
+        if file:
+            file.save(os.path.join(app.root_path+'\ManifestFolder', filename))
         return redirect(url_for('auth.unload_load'))
 
     return app
